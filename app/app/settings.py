00db82d9a8de05c01261c15160c45bf9b693d0cb
FaizanAliAbdulali <faizan.abdulali@koderlabs.com>
2023-10-05 21:37:21 +0500

    [ADD]: Recipe API code started

--- app/app/settings.py
+++ app/app/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'drf_spectacular',
 
     'user',
+    'recipe',
 ]
 
 MIDDLEWARE = [
---
+++ app/recipe/views.py
@@ -0,0 +1,16 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+
+from core.models import Recipe
+from recipe import serializers
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.RecipeSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Recipe.objects.all()
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user).order_by('-id')
